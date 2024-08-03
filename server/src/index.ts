121cdfb68ea8e64b881631fc750866c5d0410313
grochowp <patgro3@wp.pl>
2024-08-03 19:50:54 +0200

    chore: connected to database

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,27 @@
-console.log("Hello world!");
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
+
+mongoose
+  .connect(process.env.MONGODB_URL, {
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connetion Successfull");
+  })
+  .catch((err: any) => {
+    console.log(err.message);
+  });
